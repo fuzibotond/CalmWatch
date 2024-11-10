2b255880d5e692a4c37233edd9bc125ec3e77708
Füzi Botond <fuziboti@gmail.com>
2024-11-10 16:11:50 +0100

    Update mongodb setup

--- health_data.py
+++ health_data.py
@@ -6,7 +6,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 uri = MONGODB_URI
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
